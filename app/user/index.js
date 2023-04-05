cc217b1d22a1bd80aa5735ee892778453a23ef6b
kilik42 <marvinevins@yahoo.com>
2023-04-04 19:24:54 -0500

    working on profile page

--- app/user/index.js
+++ app/user/index.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router';
 
 const ProfilePage = () => {
+   const router = useRouter(); 
   return (
     <View style={{marginTop: 100}}>
       <Text>profilepage</Text>
-      
+      <Text> Go Back </Text>
+
     </View>
   )
 }
