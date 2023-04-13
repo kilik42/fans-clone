beb57294032faa1f265f89d2d0ce5bfde18f4c96
kilik42 <marvinevins@yahoo.com>
2023-04-12 21:08:00 -0500

    adding a post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,34 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { Entypo, AntDesign, FontAwesome5 } from '@expo/vector-icons';
+import { useEffect, useState } from 'react';
 
 const Post = ({post}) => {
   return (
-    <View>
-      <View style={{flexDirection:'row'}}>
+    <View  style={{marginVertical: 15 }}>
+      <View style={{flexDirection:'row', alignItems: 'center'}}>
         <Image source = {post.User.avatar} style={{width:50, height:50, borderRadius:25}} />
         <View style={{marginLeft:10}}>
-          
+
           </View>
-        <Text>{post.User.name}</Text>
+        <Text
+        style={{fontWeight: '600',fontSize: 16,  marginBottom: 3 }}
+        >{post.User.name}</Text>
         <Text >@{post.User.handle}</Text>
       </View>
 
+      <View style={{marginLeft: 'auto',
+       flexDirection:'row', 
+       alignItems: 'center'}}>
+        <Text 
+        style={{marginRight: 3, color: 'gray'}}
+        
+        >{post.caption}</Text>
+        <Entypo name="dots-three-vertical" size={24} color="black" />
+
+      </View>
+
+      <Text style={{margin: 10, lineHeight: 18}}>{post.text}</Text>
       <Image source={post.image} style={{width:'100%', aspectRatio: 1}} />
     </View>
   )
